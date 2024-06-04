a751597c515533eba3839845df2382f4bcdd66b3
marchfirst01 <marchfirst3113@gmail.com>
2024-06-04 13:43:14 +0900

    feat: add search image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import brushworkLogo from './(common)/brushwork_logo.png';
+import search from './(common)/search.svg';
 
 const IMAGES = {
   brushworkLogo,
+  search,
 };
 
 export default IMAGES;
