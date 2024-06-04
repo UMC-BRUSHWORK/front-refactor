e9c94782e767b76ed250267df01b84066eb6b2f6
marchfirst01 <marchfirst3113@gmail.com>
2024-06-04 15:36:50 +0900

    chore: eslint prettier 적용

--- src/components/(common)/header/Logo.jsx
+++ src/components/(common)/header/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import IMAGES from '../../../assets';
 import { useNavigate } from 'react-router-dom';
+import IMAGES from '../../../assets';
 
 export default function Logo() {
   const navigate = useNavigate();
