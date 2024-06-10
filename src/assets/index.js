ab11f62b0abce81461a0c66b5ddb5e1fe1a86fb4
marchfirst01 <marchfirst3113@gmail.com>
2024-06-10 14:41:28 +0900

    feat: add navigation icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,25 @@
 import brushworkLogo from './(common)/brushwork_logo.png';
 import search from './(common)/search.svg';
-import arrowBack from './(common)/arrow_back.svg'
-import notif from './(common)/notif.svg'
+import arrowBack from './(common)/arrow_back.svg';
+import notif from './(common)/notif.svg';
+
+import home from './(common)/home.svg';
+import favorite from './(common)/favorite.svg';
+import post from './(common)/post.svg';
+import chat from './(common)/chat.svg';
+import my from './(common)/my.svg';
 
 const IMAGES = {
   brushworkLogo,
   search,
   arrowBack,
-  notif
+  notif,
+
+  home,
+  favorite,
+  post,
+  chat,
+  my,
 };
 
 export default IMAGES;
