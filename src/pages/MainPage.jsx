11f6c92f0a7e305ffb8280590a28f083a6fb3f6a
marchfirst01 <marchfirst3113@gmail.com>
2024-06-10 12:22:35 +0900

    chore: main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Back from '../components/(common)/header/Back';
 
 export default function MainPage() {
   return (
-    <div>main page
-      <Back color='black' />
-    </div>
+    <div>main page</div>
   );
 }
