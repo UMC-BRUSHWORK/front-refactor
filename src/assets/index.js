042dfd70b0c0b9731034687eb74bb043a02bd07c
marchfirst01 <marchfirst3113@gmail.com>
2024-06-10 11:52:51 +0900

    feat: add notif image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import brushworkLogo from './(common)/brushwork_logo.png';
 import search from './(common)/search.svg';
+import notif from './(common)/notif.svg'
 
 const IMAGES = {
   brushworkLogo,
   search,
+  notif
 };
 
 export default IMAGES;
