4b62d71c826f66bbc5084134ccc889c973712ac9
marchfirst01 <marchfirst3113@gmail.com>
2024-06-10 12:22:05 +0900

    feat: back component

--- src/components/(common)/header/Back.jsx
+++ src/components/(common)/header/Back.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import IMAGES from '../../../assets';
 
-export default function Back() {
+export default function Back({color = 'black'}) {
+  const navigate = useNavigate()
   return (
-    <div>Back</div>
+    <Image src={IMAGES.arrowBack} alt='arrowBack' onClick={() => navigate(-1)} color={color} />
   )
 }
+
+const Image = styled.img`
+  width: 11.1rem;
+  height: 3.4rem;
+  cursor: pointer;
+  filter: ${(props) => props.color === 'primary' && 'invert(32%) sepia(84%) saturate(2988%) hue-rotate(247deg) brightness(100%) contrast(91%)'};
+`;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Back from '../components/(common)/header/Back';
 
 export default function MainPage() {
   return (
-    <div>main page</div>
+    <div>main page
+      <Back color='black' />
+    </div>
   );
 }
