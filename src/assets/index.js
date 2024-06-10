0ddaf87c17f2898035d099d1bb47a359638952d4
marchfirst01 <marchfirst3113@gmail.com>
2024-06-10 12:04:33 +0900

    feat: add arrowBack image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import brushworkLogo from './(common)/brushwork_logo.png';
 import search from './(common)/search.svg';
+import arrowBack from './(common)/arrow_back.svg'
 
 const IMAGES = {
   brushworkLogo,
   search,
+  arrowBack
 };
 
 export default IMAGES;
---
+++ src/components/(common)/header/Back.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Back() {
+  return (
+    <div>Back</div>
+  )
+}
