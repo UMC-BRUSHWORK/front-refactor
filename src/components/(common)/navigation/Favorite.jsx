c2d758372bb412f317e32e48242bf773ae52ccac
marchfirst01 <marchfirst3113@gmail.com>
2024-06-10 20:33:48 +0900

    feat: favorite component

---
+++ src/components/(common)/navigation/Favorite.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import IMAGES from '../../../assets';
+
+export default function Favorite({clicked = 'true'}) {
+  const navigate = useNavigate()
+  return (
+    <Image src={IMAGES.favorite} alt='favorite' onClick={() => navigate('/')} clicked={clicked} />
+  )
+}
+
+const Image = styled.img`
+  width: 3.2rem;
+  height: 3.2rem;
+  cursor: pointer;
+  filter: ${(props) => props.clicked === 'true' && 'invert(32%) sepia(84%) saturate(5988%) hue-rotate(247deg) brightness(100%) contrast(91%)'};
+`;
